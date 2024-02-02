66013b4322d7011b973b119fcc529b1ce16e3261
ahpinder <42349096+ahpinder@users.noreply.github.com>
2024-02-02 13:12:31 -0500

    create first testbench (confirm Verilator runs)

---
+++ verilator/fma_tb.cpp
@@ -0,0 +1,9 @@
+#include "Vfma.h"
+#include "verilated.h"
+#include "iostream"
+
+int main(int argc, char** argv) {
+    Vfma* fma = new Vfma();
+    std::cout << "can run" << std::endl;
+    return 0;
+}
